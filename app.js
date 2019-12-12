bb4ed06375c08a86612e27a8983c924ac16d9395
Maryam0187 <maryamshahid0187@gmail.com>
2019-12-13 00:28:05 +0500

    initial

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello Vue!',
+        newtodo:'',
+        todos:[]
+    },
+    methods:{
+        addTodo(){
+            this.todos.push({
+                title:this.newtodo,
+                done:false
+            });
+            this.newtodo= "";
+        },
+        removeTodo(todo)
+        {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex,1);
+        },
+        allDone()
+        {
+            this.todos.forEach(todo=>
+                {
+                    todo.done=true;
+                });
+        }
+
+    }
+  })
